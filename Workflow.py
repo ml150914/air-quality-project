85985cc1c57836087c9e7933030214b83b61a086
ml150914 <mobilia.lorenzo1996@gmail.com>
2025-09-03 14:39:46 +0200

    1756903186.823682

---
+++ Workflow.py
@@ -0,0 +1,120 @@
+import csv
+import bme280
+import smbus2
+import time
+from time import sleep
+from datetime import datetime, timedelta
+from pms5003 import PMS5003
+from collections import deque
+import os
+
+CSV_FILE_LAST60MIN = 'last_60min.csv'
+CSV_FILE_LAST24H = 'last_24H.csv'
+BUFFER_LENGTH_60MIN = 120
+BUFFER_LENGTH_24H = 180
+buffer_60M = deque(maxlen = BUFFER_LENGTH_60MIN)
+buffer_24H = deque(maxlen = BUFFER_LENGTH_24H)
+
+
+def get_filename():
+	return datetime.now().strftime('%Y-%m-%d') + '.csv'	
+	
+def get_now_timestamp():
+	return datdtime.now().strftime('%Y-%m-%d %H:%M:%S')
+	
+def ensure_file_has_header(filename):
+	try:
+		with open(filename, 'x', newline= '') as f:
+			writer = csv.writer(f)
+			writer.writerow(['timestamp', 'temperature', 'humidity', 'pressure', 'pm1.0', 'pm2.5', 'pm10'])
+	except	FileExistsError:
+		pass
+
+
+# Save the data into the buffer (csv)
+def save_buffer(buffer_60M, name_file = CSV_FILE_LAST60MIN):
+	with open(name_file, mode = 'w', newline = '') as f:
+		writer = csv.writer(f)
+		writer.writerow(['timestamp', 'temperature', 'humidity', 'pressure', 'pm1.0', 'pm2.5', 'pm10'])
+		for row in buffer_60M:
+			writer.writerow(row)
+			
+# Add data to buffer
+def add_data(buffer_60M, data):
+	buffer_60M.append(data)
+
+def git_push(filname, time):
+	#os.system(f'cp {filename} air-quality-project'}
+	os.system('git add .')
+	os.system(f'git commit -m "{time}"')
+	os.system('git push origin main')
+
+# initialize 
+current_date = datetime.now().date()
+filename = get_filename()
+ensure_file_has_header(filename)
+
+# read bme280
+port = 1
+address = 0x76 # Adafruit BME280 address. Other BME280s may be different
+bus = smbus2.SMBus(port)
+bme280.load_calibration_params(bus,address)
+
+# read PMS5003
+pms5003 = PMS5003(device="/dev/ttyAMA0", baudrate=9600)
+
+start_time = time.time()
+time_workflow = time.time()
+try:
+	while True:
+		now = datetime.now()
+		timestamp = now.strftime('%Y-%m-%d %H:%M:%S')
+		bme280_data = bme280.sample(bus,address)
+		humidity  = bme280_data.humidity
+		pressure  = bme280_data.pressure
+		ambient_temperature = bme280_data.temperature
+		print(f'Humidity: {humidity:.2f} | Pression:{pressure:.2f} | Room Temperature:{ambient_temperature:.2f}')
+		data = pms5003.read()
+		print(f'PM1.0 {data.pm_ug_per_m3(1.0)} | PM2.5 {data.pm_ug_per_m3(2.5)} | PM10: {data.pm_ug_per_m3(10)}')
+		print('\n')
+		
+		data_buffer = [timestamp, humidity, pressure, ambient_temperature, data.pm_ug_per_m3(1.0), data.pm_ug_per_m3(2.5), data.pm_ug_per_m3(10)]
+		# Check id the day changed
+		if now.date() != current_date:
+			current_date = now.date()
+			filename = get_filename()
+			ensure_file_has_header(filename)
+		
+		if now.date() != current_date:
+			current_date = now.date()
+			filename = get_filename()
+			ensure_file_has_header(filename)
+		
+		with open(filename, 'a', newline='') as f:
+			writer = csv.writer(f)
+			writer.writerow([timestamp, ambient_temperature, humidity, pressure, data.pm_ug_per_m3(1.0), data.pm_ug_per_m3(2.5), data.pm_ug_per_m3(10)])
+		
+		#60 min buffer
+		current_time = time.time() - start_time
+		if current_time > BUFFER_LENGTH_60MIN:
+			#ensure_file_has_header(CSV_FILE_LAST60MIN)
+			save_buffer(buffer_60M, CSV_FILE_LAST60MIN)
+			# clean the buffer
+			cleaned_buffer = deque(maxlen = BUFFER_LENGTH_60MIN)
+			buffer_60M = cleaned_buffer
+			start_time = time.time()
+			git_push(CSV_FILE_LAST60MIN, start_time)
+		
+		if current_time > BUFFER_LENGTH_24H:
+			save_buffer(buffer_24H, CSV_FILE_LAST24H)
+			# clean the buffer
+			cleaned_buffer = deque(maxlen = BUFFER_LENGTH_24H)
+			buffer_24H = cleaned_buffer
+			start_time = time.time()
+			git_push(CSV_FILE_LAST60MIN, start_time)
+		add_data(buffer_60M, data_buffer)
+		add_data(buffer_24H, data_buffer)
+		sleep(60)
+
+except KeyboardInterrupt:
+	pass
