ec0880b34525869c17e381a37a361b06b29706f4
ml150914 <mobilia.lorenzo1996@gmail.com>
2025-09-07 10:45:49 +0200

    1757234448.8670433

--- Workflow.py
+++ Workflow.py
@@ -32,16 +32,16 @@ def ensure_file_has_header(filename):
 
 
 # Save the data into the buffer (csv)
-def save_buffer(buffer_60M, name_file):
+def save_buffer(buffer_tmp, name_file):
 	with open('data/' + name_file, mode = 'w', newline = '') as f:
 		writer = csv.writer(f)
 		writer.writerow(['timestamp', 'temperature', 'humidity', 'pressure', 'pm1.0', 'pm2.5', 'pm10'])
-		for row in buffer_60M:
+		for row in buffer_tmp:
 			writer.writerow(row)
 			
 # Add data to buffer
-def add_data(buffer_60M, data):
-	buffer_60M.append(data)
+def add_data(buffer_tmp, data):
+	buffer_tmp.append(data)
 
 def git_push(filname, time):
 	#os.system(f'cp {filename} air-quality-project'}
@@ -64,8 +64,8 @@ bme280.load_calibration_params(bus,address)
 pms5003 = PMS5003(device="/dev/ttyAMA0", baudrate=9600)
 
 start_time60min = time.time()
-start_time24H = time.time()
-time_workflow = time.time()
+start_time24H = start_time60min 
+time_workflow = start_time60min 
 try:
 	while True:
 		now = datetime.now()
@@ -74,8 +74,8 @@ try:
 		humidity  = bme280_data.humidity
 		pressure  = bme280_data.pressure
 		ambient_temperature = bme280_data.temperature
-		print(f'Humidity: {humidity:.2f} | Pression:{pressure:.2f} | Room Temperature:{ambient_temperature:.2f}')
 		data = pms5003.read()
+		print(f'Humidity: {humidity:.2f} | Pression:{pressure:.2f} | Room Temperature:{ambient_temperature:.2f}')
 		print(f'PM1.0 {data.pm_ug_per_m3(1.0)} | PM2.5 {data.pm_ug_per_m3(2.5)} | PM10: {data.pm_ug_per_m3(10)}')
 		print('\n')
 		
@@ -86,24 +86,18 @@ try:
 			filename = get_filename()
 			ensure_file_has_header(filename)
 		
-		if now.date() != current_date:
-			current_date = now.date()
-			filename = get_filename()
-			ensure_file_has_header(filename)
-		
-		with open(filename, 'a', newline='') as f:
+		with open('data/' + filename, 'a', newline='') as f:
 			writer = csv.writer(f)
 			writer.writerow([timestamp, ambient_temperature, humidity, pressure, data.pm_ug_per_m3(1.0), data.pm_ug_per_m3(2.5), data.pm_ug_per_m3(10)])
 		
 		#60 min buffer
 		current_time = time.time() - start_time60min
 		if current_time > BUFFER_LENGTH_60MIN:
-			#ensure_file_has_header(CSV_FILE_LAST60MIN)
 			save_buffer(buffer_60M, CSV_FILE_LAST60MIN)
 			# clean the buffer
 			cleaned_buffer = deque(maxlen = BUFFER_LENGTH_60MIN)
 			buffer_60M = cleaned_buffer
-			start_time_60min = time.time()
+			start_time_60min = current_time
 			git_push(CSV_FILE_LAST60MIN, start_time60min)
 		
 		current_time = time.time() - start_time24H
@@ -112,8 +106,9 @@ try:
 			# clean the buffer
 			cleaned_buffer = deque(maxlen = BUFFER_LENGTH_24H)
 			buffer_24H = cleaned_buffer
-			start_time24H = time.time()
+			start_time24H = current_time
 			git_push(CSV_FILE_LAST24H, start_time24H)
+		
 		add_data(buffer_60M, data_buffer)
 		add_data(buffer_24H, data_buffer)
 		sleep(60)
